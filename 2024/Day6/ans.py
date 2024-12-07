79458647699364a4aa364b068ff53f59c6453e47
oliverhnat <oliver.hnat@gmail.com>
2024-12-07 12:27:29 +0100

    feat(day6): part 2, not done

--- 2024/Day6/ans.py
+++ 2024/Day6/ans.py
@@ -14,6 +14,17 @@ class Directions(Enum):
     LEFT = 2
     RIGHT = 3
 
+    def __str__(self) -> str:
+        match self:
+            case self.UP:
+                return "U"
+            case self.DOWN:
+                return "D"
+            case self.LEFT:
+                return "L"
+            case self.RIGHT:
+                return "R"
+
 
 def part1():
     matrix = []
@@ -41,6 +52,67 @@ def part1():
     for line in matrix:
         countX += line.count("X")
     print(countX)
+
+
+def getMatrixFromInput(input):
+    startX = 0
+    startY = 0
+    matrix = []
+    for i, line in enumerate(input):
+        matrix.append(list(line))
+        if '^' in line:
+            startX = line.index('^')
+            startY = i
+            matrix[startY][startX] = "X"
+    return matrix, startX, startY
+
+
+def part2():
+    matrix, startX, startY = getMatrixFromInput(input)
+    x, y = startX, startY
+    direction = Directions.UP
+    originalPath = []
+    while validCoordinates(x, y, matrix):
+        newX, newY = move(x, y, direction)
+        if not validCoordinates(newX, newY, matrix):
+            break
+        if matrix[newY][newX] in (".", "X"):
+            matrix[newY][newX] = "X"
+            x, y = newX, newY
+            originalPath.append((x, y))
+        elif matrix[newY][newX] == "#":
+            direction = switchDirection(direction)
+    loop = 0
+    # for i, (givenX, givenY) in enumerate(originalPath):
+            # print(i)
+    for givenY in range(len(matrix)):
+        for givenX in range(len(matrix[givenY])):
+            # print(givenY, givenX)
+            x, y = startX, startY
+            if (x,y) == (givenX, givenY):
+                continue
+            elif (givenY,givenX) not in originalPath:
+                continue
+            direction = Directions.UP
+            newMatrix, _, _ = getMatrixFromInput(input)
+            newMatrix[givenX][givenY] = "#"
+            visited = []
+            while True:
+                newX, newY = move(x, y, direction)
+                if not validCoordinates(newX, newY, newMatrix):
+                    break
+                if (newY, newX, direction) in visited:
+                    loop += 1
+                    # print("LOOP: ", x, y, direction)
+                    # strprint(newMatrix)
+                    break
+                if newMatrix[newY][newX] in (".", "X"):
+                    newMatrix[newY][newX] = "X"
+                    visited.append((newY, newX, direction))
+                    x, y = newX, newY
+                elif newMatrix[newY][newX] == "#":
+                    direction = switchDirection(direction)
+    print(loop)
             
 
 def validCoordinates(x: int, y: int, matrix: List[List[str]]) -> bool:
@@ -73,3 +145,5 @@ def strprint(lines):
     for line in lines:
         print("".join(line))
     print()
+
+part2()
