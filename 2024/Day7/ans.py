b74aace61c4c3eeb4aacc187c38c7bbd186906da
Oliver <oliver@elnino.tech>
2025-09-18 16:38:57 +0200

    feat(day7): part 2

--- 2024/Day7/ans.py
+++ 2024/Day7/ans.py
@@ -31,4 +31,32 @@ def getSum(expected, result, values):
     return result1 or result2
         
 
-part1()
+def part2():
+    res = 0
+    # input = {int(key): [int(v) for v in val.split(" ")] for key, val in input.items()}
+    inputs = []
+    for i in input:
+        parts = re.findall(r'\d+', i)
+        inputs.append({int(parts[0]): [int(v) for v in parts[1:]]})
+    for line in inputs:
+        r = list(line.keys())[0]
+        vals = list(line.values())[0]
+        if getSum2(r, 0, vals):
+            res += r
+    print(res)
+
+def getSum2(expected, result, values):
+    if expected < result:
+        return False
+    if len(values) == 0:
+        return expected == result
+    result1 = getSum2(expected, result+values[0], values[1:])
+    if result1:
+        return True
+    result2 = getSum2(expected, result*values[0], values[1:])
+    if result2:
+        return True
+    result3 = getSum2(expected, int(str(result) + str(values[0])), values[1:])
+    return result3
+
+part2()
