45f6ba8da8c4409a4ee4322a8287d8c8b6bfa13b
oliverhnat <oliver.hnat@gmail.com>
2025-12-06 18:18:51 +0100

    fix(day1): finish part 2

--- 2025/Day1/ans.py
+++ 2025/Day1/ans.py
@@ -4,13 +4,14 @@ def parseInput(file):
     with open(file, "r") as f:
         return f.read()
 
+input = parseInput("example.in").split("\n")[:-1]
 input = parseInput("input.in").split("\n")[:-1]
 
 def part1():
     dial = 50
     pointAtZero = 0
     for line in input:
-        side = line[0]
+        # side = line[0]
         turn = int(line[1:])
         if side == "L":
             dial -= turn
@@ -21,5 +22,28 @@ def part1():
             pointAtZero += 1
     print(pointAtZero)
 
-part1()
+def part2():
+    dial = 50
+    pointAtZero = 0
+    for line in input:
+        side = line[0]
+        turn = int(line[1:])
+        originalDial = dial
+        while turn >= 100:
+            turn -= 100
+            pointAtZero += 1
+        if side == "L":
+            dial -= turn
+        else:
+            dial += turn
 
+        if dial == 0:
+            pointAtZero += 1
+        if dial >= 100:
+            dial -= 100
+            pointAtZero += 1
+        if dial < 0:
+            dial += 100
+            if originalDial != 0:
+                pointAtZero += 1
+    print(pointAtZero)
