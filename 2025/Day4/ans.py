f95c0d2ef645c92907062ec8bc5d4c2d590740ca
oliverhnat <oliver.hnat@gmail.com>
2025-12-17 16:33:10 +0100

    feat(day4): part 2

--- 2025/Day4/ans.py
+++ 2025/Day4/ans.py
@@ -35,4 +35,42 @@ def getGrid(grid, coordinates):
         return None
     return grid[x][y]
 
-part1()
+def part2():
+    grid = [[i for i in list(line)] for line in input]
+    adjacentCoordinates = [
+        (-1, -1), (-1, 0), (-1, 1),
+        (0, -1),          (0, 1),
+        (1, -1),  (1, 0),  (1, 1)
+    ]
+    accessibleRols = 0
+    x = 0
+    while x < len(grid):
+        y = 0
+        while y < len(grid[x]):
+            value = grid[x][y]
+            if value != "@":
+                y += 1
+                continue
+            adjacenentRolls = 0
+            for a, b in adjacentCoordinates:
+                adj = getGrid(grid, (x+a,y+b))
+                if adj is not None and adj == "@":
+                    adjacenentRolls += 1
+            if adjacenentRolls < 4:
+                grid[x][y] = "."
+                accessibleRols += 1
+                x = 0
+                y = 0
+                break
+            y += 1
+        else:
+            x += 1
+            continue
+        x = 0
+    print(accessibleRols)
+
+def strprint(lines):
+    for line in lines:
+        print("".join(line))
+    print()
+part2()
