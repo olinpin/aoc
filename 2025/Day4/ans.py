8cb489dc26764a1457b7664537999c6f366891fd
oliverhnat <oliver.hnat@gmail.com>
2025-12-17 16:07:26 +0100

    feat(day4): part 1

---
+++ 2025/Day4/ans.py
@@ -0,0 +1,38 @@
+def parseInput(file):
+    with open(file, "r") as f:
+        return f.read()
+
+input = parseInput("example.in").split("\n")[:-1]
+input = parseInput("input.in").split("\n")[:-1]
+
+def part1():
+    grid = [[i for i in list(line)] for line in input]
+    adjacentCoordinates = [
+        (-1, -1), (-1, 0), (-1, 1),
+        (0, -1),          (0, 1),
+        (1, -1),  (1, 0),  (1, 1)
+    ]
+    accessibleRols = 0
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            value = grid[x][y]
+            if value != "@":
+                continue
+            adjacenentRolls = 0
+            for a, b in adjacentCoordinates:
+                adj = getGrid(grid, (x+a,y+b))
+                if adj is not None and adj == "@":
+                    adjacenentRolls += 1
+            if adjacenentRolls < 4:
+                accessibleRols += 1
+    print(accessibleRols)
+
+
+def getGrid(grid, coordinates):
+    x = coordinates[0]
+    y = coordinates[1]
+    if x < 0 or y < 0 or x >= len(grid) or y >= len(grid[x]):
+        return None
+    return grid[x][y]
+
+part1()
